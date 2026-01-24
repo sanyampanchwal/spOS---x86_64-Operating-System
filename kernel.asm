29c25c9324d60c356bfe1321e30d8c379bc8d47b
sanyampanchwal <sanyampanchwal99@gmail.com>
2026-01-24 13:33:53 +0530

    Added interrupt handling for ring3 - user , diealt with interrupt problems and spurious interrupts

--- kernel.asm
+++ kernel.asm
@@ -3,24 +3,31 @@
 
 start:
     mov rdi,Idt
-    
     mov rax,Handler0
-    mov [rdi],ax
-    shr rax,16
-    mov [rdi+6],ax
-    shr rax,16
-    mov [rdi+8],eax
+    call SetHandler
 
     mov rax,Timer
-    add rdi,32*16
-    mov [rdi],ax
-    shr rax,16
-    mov [rdi+6],ax
-    shr rax,16
-    mov [rdi+8],eax
+    mov rdi,Idt+32*16
+    call SetHandler
 
+    mov rdi,Idt+32*16+7*16
+    mov rax,SIRQ
+    call SetHandler
+    
     lgdt [Gdt64Ptr]
     lidt [IdtPtr]
+
+SetTss:
+    mov rax,Tss
+    mov [TssDesc+2],ax
+    shr rax,16
+    mov [TssDesc+4],al
+    shr rax,8
+    mov [TssDesc+7],al
+    shr rax,8
+    mov [TssDesc+8],eax
+    mov ax,0x20
+    ltr ax
 
     push 8
     push KernelEntry
@@ -64,11 +71,29 @@ InitPIC:
     mov al,11111111b
     out 0xa1,al
 
-    sti
+    push 0x18|3
+    push 0x7c00
+    push 0x202
+    push 0x10|3
+    push UserEntry
+    iretq
 
 End:
     hlt
     jmp End
+
+SetHandler:
+    mov [rdi],ax
+    shr rax,16
+    mov [rdi+6],ax
+    shr rax,16
+    mov [rdi+8],eax
+    ret
+
+UserEntry:
+    inc byte[0xb8010]
+    mov byte[0xb8011],0xF
+    jmp UserEntry
 
 Handler0:
     push rax
@@ -127,9 +152,11 @@ Timer:
     push r14
     push r15
 
-    mov byte[0xb8020],'T'
+    inc byte[0xb8020]
     mov byte[0xb8021],0xe
-    jmp End
+    
+    mov al,0x20
+    out 0x20,al
    
     pop	r15
     pop	r14
@@ -149,9 +176,67 @@ Timer:
 
     iretq
 
+SIRQ:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov al,11
+    out 0x20,al
+    in al,0x20
+
+    test al,(1<<7)
+    jz .end
+
+    mov al,0x20
+    out 0x20,al
+
+.end:
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
+
 Gdt64:
     dq 0
     dq 0x0020980000000000
+    dq 0x0020f80000000000
+    dq 0x0000f20000000000
+TssDesc:
+    dw TssLen-1
+    dw 0
+    db 0
+    db 0x89
+    db 0
+    db 0
+    dq 0
+
 
 Gdt64Len: equ $-Gdt64
 
@@ -175,3 +260,11 @@ IdtLen: equ $-Idt
 
 IdtPtr: dw IdtLen-1
         dq Idt
+
+Tss:
+    dd 0
+    dq 0x150000
+    times 88 db 0
+    dd TssLen
+
+TssLen: equ $-Tss
