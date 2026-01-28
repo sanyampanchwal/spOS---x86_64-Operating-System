67ba5ddde2fa9d653cc757bb4ca7edd0c8ee6b15
sanyampanchwal <sanyampanchwal99@gmail.com>
2026-01-29 00:44:34 +0530

    integrate C kernel and build system
    Migrated build process to use GCC/LD with custom linker script (link.lds).
    
    Added -mno-red-zone and -ffreestanding flags for kernel safety.
    
    Defined memory layout in link.lds with origin at 0x200000.
    
    Refactored kernel.asm to serve as the crt0 (C Run-Time) entry point.
    
    Implemented stack setup and handover to KMain() in C.
    
    Added basic video memory driver in main.c.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 nasm -f bin -o boot.bin boot.asm
 nasm -f bin -o loader.bin loader.asm
-nasm -f bin -o kernel.bin kernel.asm
+nasm -f elf64 -o kernel.o kernel.asm
+
+x86_64-elf-gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c main.c -o main.o
+x86_64-elf-ld -nostdlib -T link.lds -o kernel kernel.o main.o
+x86_64-elf-objcopy -O binary kernel kernel.bin
+
 dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
 dd if=kernel.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
